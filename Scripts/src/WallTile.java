a9a886601bcbbad4b2a43654fa71d95e0db398d2
Nielser <mrnielsvtz@gmail.com>
2019-08-21 13:34:53 +0200

    Auto stash before merge of "Movement" and "origin/UI"

--- Scripts/src/WallTile.java
+++ Scripts/src/WallTile.java
@@ -2,7 +2,7 @@ import javafx.scene.paint.Color;
 
 public class WallTile extends Tile {
     public WallTile(int positionX, int positionY, int pixelSize) {
-        super(positionX, positionY, pixelSize);
+        super(positionX, positionY, pixelSize );
         this.color = Color.BLUE; //in fx umwandeln
     }
 }
